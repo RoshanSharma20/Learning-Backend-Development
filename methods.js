4e9ad76f2b1ab9698239d5938af6c694af878526
Roshan Sharma <roshan.s4d@gmail.com>
2023-04-11 02:23:05 +0530

    mounting in express

--- methods.js
+++ methods.js
@@ -5,62 +5,80 @@ app.use(express.json());
 app.listen(3000);
 
 
+//mounting in express ->used to segregate same routes with different http methods,resembling as small mini applications
+let users = [
+    {
+        "id": 1,
+        "name": "superman"
+    },
+    {
+        "id": 2,
+        "name": "batman"
+    },
+    {
+        "id": 3,
+        "name": "robin"
+    }
+];
+
+//create a router object
+const UserRouter = express.Router();
+
+//specifying the base url and determining which router to user for the particular base url
+app.use('/user', UserRouter);
+
+UserRouter
+    .route('/')
+    .get(getUser)
+    .post(postUser)
+    .patch(updateUser)
+    .delete(deleteUser);
 
 
-//creating an user object to send as a response object
-// let users = {};
-// app.get('/user', (req, res) => {
-//     res.send(users);
-// });
+UserRouter.route('/:id').get(getUserById);
 
+function getUser(req, res) {
+    res.json(users);
+}
 
+function postUser(req, res) {
 
-//understanding params and queries in url
-//parameters in requests
-//we can use maximum upto 5 parameters
-app.get('/user/:id', (req, res) => {
-    console.log(req.params.id);
-    res.send('hello world');
-})
-
-
-//queries in url
-app.get('/user', (req, res) => {
-    console.log(req.query);
-    res.send('hello');
-})
-
-
-//using the post method to send data from front-end to server
-app.post('/user', (req, res) => {
     console.log(req.body);
-    users = req.body;
+    // users = req.body;
     //a response need to be sent back
     res.json({
         message: "your request has been submitted",
         users: req.body
+    });
+}
+function updateUser(req, res) {
+    console.log(req.body);
+    let dataToBeUpdated = req.body;
+    //using the for in loop to update the user object
+    for (key in dataToBeUpdated) {
+        users[key] = dataToBeUpdated[key];
+    }
+    //a response needs to be sent back
+    res.json({
+        message: "data has been updated successfully"
     })
+}
+function deleteUser(req, res) {
+    res.json({
+        message: "content has been deleted"
+    })
+}
 
-    //using the patch method to update the user object
-    app.patch('/user', (req, res) => {
-        console.log(req.body);
-        let dataToBeUpdated = req.body;
-        //using the for in loop to update the user object
-        for (key in dataToBeUpdated) {
-            users[key] = dataToBeUpdated[key];
+function getUserById(req, res) {
+    const id = req.params.id;
+    let obj = {};
+    for (let i = 0; i < users.length; ++i) {
+        if (users[i]['id'] == id) {
+            obj = users[i];
         }
-        //a response needs to be sent back
-        res.json({
-            message: "data has been updated successfully"
-        })
+    }
+    res.json({
+        message: "req received",
+        data: obj
     })
-
-    //using the delete method to delete the contents of the users object
-    app.delete('/user', (req, res) => {
-        users = {};
-        res.json({
-            message: "content has been deleted"
-        })
-    })
-
-})
+}
